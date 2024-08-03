49bc3b97b707b7997f4ab6cb486216b51bda09f4
ngan <vutran@sheridancollege.ca>
2024-08-02 22:56:55 -0400

    React version is done

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,6 +1,54 @@
+import { useState, useEffect, useRef } from "react"
+
 function App() {
+  const trackRef = useRef(null)
+  const [mouseDownAt, setMouseDownAt] = useState(0)
+  const [prevPercentage, setPrevPercentage] = useState(0)
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      if (mouseDownAt === 0) return
+
+      const mouseDelta = mouseDownAt - e.clientX
+      const maxDelta = window.innerWidth / 2
+
+      const percentage = (mouseDelta / maxDelta) * -100
+      const nextPercentageUnconstrained = prevPercentage + percentage
+      const nextPercentage = Math.max(
+        Math.min(nextPercentageUnconstrained, 0),
+        -100
+      )
+
+      trackRef.current.dataset.percentage = nextPercentage
+      trackRef.current.style.transform = `translate(${nextPercentage}%, -50%)`
+    }
+
+    const handleMouseDown = (e) => {
+      setMouseDownAt(e.clientX)
+    }
+
+    const handleMouseUp = () => {
+      setMouseDownAt(0)
+      setPrevPercentage(parseFloat(trackRef.current.dataset.percentage))
+    }
+
+    window.addEventListener("mousemove", handleMouseMove)
+    window.addEventListener("mousedown", handleMouseDown)
+    window.addEventListener("mouseup", handleMouseUp)
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove)
+      window.removeEventListener("mousedown", handleMouseDown)
+      window.removeEventListener("mouseup", handleMouseUp)
+    }
+  }, [mouseDownAt, prevPercentage])
   return (
-    <div id="image-track" data-mouse-down-at="0" data-prev-percentage="0">
+    <div
+      id="image-track"
+      ref={trackRef}
+      data-mouse-down-at="0"
+      data-prev-percentage="0"
+    >
       <img
         class="image"
         src="https://images.unsplash.com/photo-1524781289445-ddf8f5695861?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
